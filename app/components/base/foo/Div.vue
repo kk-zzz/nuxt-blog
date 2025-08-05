ad2c31510458c01bdc959ff45d3e765c2e050389
kk.z <kk.z@qycocu.com>
2025-08-05 14:32:05 +0800

    使用动态组件引入方式

---
+++ app/components/base/foo/Div.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Div</div>
+</template>
+  
+<script setup lang='ts'>
+  
+</script>
+  
+<style>
+  
+</style>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <BaseFooButton />
+    <button @click="isDiv = !isDiv">toggle {{ isDiv ? 'button' : 'div' }}</button>
+    <component :is="isDiv ? BaseFooDiv : MyButton" />
   </div>
 </template>
   
 <script setup lang='ts'>
+/**
+ * 若是动态组件，component，两种引入组件方式
+ * 1. 使用 resolveComponent(组件名称) 来获取组件 
+ * 2. import { 组件名称, ... } from '#components' 中引入
+ */
+import { BaseFooDiv } from '#components'
+const MyButton = resolveComponent('BaseFooButton')
+const isDiv = ref(false)
 </script>
   
 <style lang="scss" scoped>
